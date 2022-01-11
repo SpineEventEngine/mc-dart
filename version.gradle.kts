7561d9c5ba675eebf389cd7b2e2a2700fbb75a36
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2022-01-11 17:16:17 +0200

    Update version

--- version.gradle.kts
+++ version.gradle.kts
@@ -28,4 +28,4 @@ val baseVersion by extra("2.0.0-SNAPSHOT.80")
 val toolBaseVersion by extra("2.0.0-SNAPSHOT.85")
 val mcVersion by extra("2.0.0-SNAPSHOT.87")
 
-val versionToPublish by extra("2.0.0-SNAPSHOT.87")
+val versionToPublish by extra("2.0.0-SNAPSHOT.88")
